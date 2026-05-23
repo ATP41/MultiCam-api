9bf9049781f9411e638e611cbbb4123959e699de
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2026-05-23 22:00:57 +0700

    refactor: изменение clob -> text, bigint auto inkrement -> big serial

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS users (
-    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username       VARCHAR(255) NOT NULL ,
+    id                BIGSERIAL PRIMARY KEY,
+    username          VARCHAR(255) NOT NULL ,
     hashed_password   VARCHAR(255) NOT NULL,
-    email      VARCHAR(255) UNIQUE,
-    is_guest   BOOL DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT NOW()
+    email             VARCHAR(255) UNIQUE,
+    is_guest          BOOL DEFAULT FALSE,
+    created_at        TIMESTAMP DEFAULT NOW()
     );
 
 CREATE TABLE IF NOT EXISTS saved_result (
-    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    image_url  VARCHAR(1024),
-    json_data  CLOB,
-    user_id BIGINT,
-    category   VARCHAR(100),
-    created_at TIMESTAMP DEFAULT NOW()
+    id                BIGSERIAL PRIMARY KEY,
+    image_url         VARCHAR(1024),
+    json_data         TEXT,
+    user_id           BIGINT REFERENCES users(id),
+    category          VARCHAR(100),
+    created_at        TIMESTAMP DEFAULT NOW()
 );
